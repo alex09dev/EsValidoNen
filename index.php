c448c4205c343cef818b0be6377e8202ca9328f0
alex09dev <alexclaramonte@escolaculturalbadalona.com>
2024-04-06 22:26:57 +0200

    change title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flip</title>
+    <title>Es valido nen??</title>
 </head>
 
 <body>
